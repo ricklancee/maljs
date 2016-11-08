26ac0d2bee5d78ac0d8b419cfc8c6445b9c8227b
Rick Lancee <rickondraw@gmail.com>
2016-11-08 20:27:01 +0100

    update build

--- es5/mal.js
+++ es5/mal.js
@@ -4,222 +4,289 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var MALjs = function () {
-  function MALjs(user, password) {
-    var _this = this;
+(function () {
+  var root = this;
+  var isNode = typeof exports !== 'undefined';
+  var xml2js;
+  var request;
 
-    _classCallCheck(this, MALjs);
-
-    if (!user || !password) {
-      throw new Error('MALjs requires a myanimelist.net username and password.');
-    }
-
-    this._user = user;
-    this._password = password;
-    this._base = 'https://myanimelist.net';
-    this._parser = new DOMParser();
-
-    this.anime = {
-      search: function search(query) {
-        return _this.search(query, 'anime');
-      },
-      list: function list() {
-        return _this.list('anime');
-      },
-      add: function add(id, data) {
-        return _this.add(id, data, 'anime');
-      },
-      update: function update(id, data) {
-        return _this.update(id, data, 'anime');
-      },
-      delete: function _delete(id, data) {
-        return _this.delete(id, 'anime');
-      }
-    };
-
-    this.manga = {
-      search: function search(query) {
-        return _this.search(query, 'manga');
-      },
-      list: function list() {
-        return _this.list('manga');
-      },
-      add: function add(id, data) {
-        return _this.add(id, data, 'manga');
-      },
-      update: function update(id, data) {
-        return _this.update(id, data, 'manga');
-      },
-      delete: function _delete(id, data) {
-        return _this.delete(id, 'manga');
-      }
-    };
+  if (isNode) {
+    xml2js = require('xml2js');
+    request = require('request');
   }
 
-  _createClass(MALjs, [{
-    key: 'search',
-    value: function search(query, type) {
-      this._checkType(type);
+  var MALjs = function () {
+    function MALjs(user, password) {
+      var _this = this;
 
-      return this._get(this._base + '/api/' + type + '/search.xml?q=' + query);
-    }
-  }, {
-    key: 'list',
-    value: function list(type) {
-      this._checkType(type);
-      return this._get(this._base + '/malappinfo.php?u=' + this._user + '&status=all&type=' + type);
-    }
-  }, {
-    key: 'add',
-    value: function add(id, data, type) {
-      this._checkType(type);
+      _classCallCheck(this, MALjs);
 
-      if (!data.entry) {
-        data = { entry: data };
+      if (!user || !password) {
+        throw new Error('MALjs requires a myanimelist.net username and password.');
       }
 
-      return this._post(this._base + '/api/' + type + 'list/add/' + id + '.xml', data);
-    }
-  }, {
-    key: 'update',
-    value: function update(id, data, type) {
-      this._checkType(type);
+      this._user = user;
+      this._password = password;
+      this._base = 'https://myanimelist.net';
 
-      if (!data.entry) {
-        data = { entry: data };
+      this._parser = isNode ? new xml2js.Parser() : new DOMParser();
+
+      this.anime = {
+        search: function search(query) {
+          return _this.search(query, 'anime');
+        },
+        list: function list() {
+          return _this.list('anime');
+        },
+        add: function add(id, data) {
+          return _this.add(id, data, 'anime');
+        },
+        update: function update(id, data) {
+          return _this.update(id, data, 'anime');
+        },
+        delete: function _delete(id, data) {
+          return _this.delete(id, 'anime');
+        }
+      };
+
+      this.manga = {
+        search: function search(query) {
+          return _this.search(query, 'manga');
+        },
+        list: function list() {
+          return _this.list('manga');
+        },
+        add: function add(id, data) {
+          return _this.add(id, data, 'manga');
+        },
+        update: function update(id, data) {
+          return _this.update(id, data, 'manga');
+        },
+        delete: function _delete(id, data) {
+          return _this.delete(id, 'manga');
+        }
+      };
+    }
+
+    _createClass(MALjs, [{
+      key: 'search',
+      value: function search(query, type) {
+        this._checkType(type);
+
+        return this._get(this._base + '/api/' + type + '/search.xml?q=' + query);
       }
-
-      return this._post(this._base + '/api/' + type + 'list/update/' + id + '.xml', data);
-    }
-  }, {
-    key: 'delete',
-    value: function _delete(id, type) {
-      this._checkType(type);
-      return this._post(this._base + '/api/' + type + 'list/delete/' + id + '.xml');
-    }
-  }, {
-    key: 'verifyCredentials',
-    value: function verifyCredentials() {
-      return this._get(this._base + '/api/account/verify_credentials.xml');
-    }
-  }, {
-    key: '_checkType',
-    value: function _checkType(type) {
-      if (type !== 'anime' && type !== 'manga') {
-        throw new Error('Only allowed types are anime and manga. incorrect type: ' + type + ' given.');
+    }, {
+      key: 'list',
+      value: function list(type) {
+        this._checkType(type);
+        return this._get(this._base + '/malappinfo.php?u=' + this._user + '&status=all&type=' + type);
       }
-    }
-  }, {
-    key: '_parseXml',
-    value: function _parseXml(xmlString) {
-      var dom = this._parser.parseFromString(xmlString, "text/xml");
+    }, {
+      key: 'add',
+      value: function add(id, data, type) {
+        this._checkType(type);
 
-      if (dom.documentElement.nodeName === "html") {
-        return false;
-      }
-
-      return dom;
-    }
-  }, {
-    key: '_toJson',
-    value: function _toJson(dom) {
-      var nodes = dom.childNodes;
-      var object = {};
-
-      for (var i = 0; i < nodes.length; i++) {
-        var node = nodes[i];
-
-        if (node.nodeName === 'myanimelist') {
-          object[node.nodeName] = {};
-        } else {
-          object[node.nodeName] = [];
+        if (!data) {
+          return;
         }
 
-        var childNodes = node.childNodes;
+        if (!data.entry) {
+          data = { entry: data };
+        }
 
-        for (var _i = 0; _i < childNodes.length; _i++) {
-          var entryNode = childNodes[_i];
-          var entryObject = {};
+        return this._post(this._base + '/api/' + type + 'list/add/' + id + '.xml', data);
+      }
+    }, {
+      key: 'update',
+      value: function update(id, data, type) {
+        this._checkType(type);
 
-          // Skip empty text nodes.
-          if (entryNode.nodeName === '#text') continue;
+        if (!data) {
+          return;
+        }
 
-          var items = entryNode.childNodes;
+        if (!data.entry) {
+          data = { entry: data };
+        }
 
-          for (var _i2 = 0; _i2 < items.length; _i2++) {
-            var item = items[_i2];
+        return this._post(this._base + '/api/' + type + 'list/update/' + id + '.xml', data);
+      }
+    }, {
+      key: 'delete',
+      value: function _delete(id, type) {
+        this._checkType(type);
+        return this._post(this._base + '/api/' + type + 'list/delete/' + id + '.xml');
+      }
+    }, {
+      key: 'verifyCredentials',
+      value: function verifyCredentials() {
+        return this._get(this._base + '/api/account/verify_credentials.xml');
+      }
+    }, {
+      key: '_checkType',
+      value: function _checkType(type) {
+        if (type !== 'anime' && type !== 'manga') {
+          throw new Error('Only allowed types are anime and manga. incorrect type: ' + type + ' given.');
+        }
+      }
+    }, {
+      key: '_xmlToJson',
+      value: function _xmlToJson(xmlString) {
+        var _this2 = this;
 
-            if (item.nodeName === '#text') continue;
+        return new Promise(function (resolve, reject) {
+          if (isNode) {
+            _this2._parser.parseString(xmlString, function (err, result) {
 
-            var value = item.innerHTML;
+              // Strip out entry prop
+              if (result.anime && result.anime.entry) {
+                var entries = result.anime.entry;
+                delete result.anime.entry;
+                result.anime = entries;
+              }
 
-            if (item.nodeName === 'id' || item.nodeName === 'episodes') {
-              value = parseInt(value, 10);
+              if (result.manga && result.manga.entry) {
+                var entries = result.manga.entry;
+                delete result.manga.entry;
+                result.manga = entries;
+              }
+
+              if (err) {
+                reject(err);
+              } else {
+                resolve(result);
+              }
+            });
+          } else {
+            var dom = _this2._parser.parseFromString(xmlString, "text/xml");
+
+            if (dom.documentElement.nodeName === "html") {
+              reject('Failed to parse xml.');
+            } else {
+              resolve(_this2._domToJson(dom));
             }
-
-            entryObject[item.nodeName] = value;
           }
+        });
+      }
+    }, {
+      key: '_domToJson',
+      value: function _domToJson(dom) {
+        var nodes = dom.childNodes;
+        var object = {};
+
+        for (var i = 0; i < nodes.length; i++) {
+          var node = nodes[i];
 
           if (node.nodeName === 'myanimelist') {
-            if (entryNode.nodeName === 'anime' || entryNode.nodeName === 'manga') {
-              if (!object[node.nodeName][entryNode.nodeName]) {
-                object[node.nodeName][entryNode.nodeName] = [];
-              }
-              object[node.nodeName][entryNode.nodeName].push(entryObject);
-            } else {
-              object[node.nodeName][entryNode.nodeName] = entryObject;
-            }
+            object[node.nodeName] = {};
           } else {
-            object[node.nodeName].push(entryObject);
+            object[node.nodeName] = [];
           }
-        }
-      }
 
-      return object;
-    }
-  }, {
-    key: '_toXml',
-    value: function _toXml(object) {
-      var xmlString = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
+          var childNodes = node.childNodes;
 
-      function getProps(obj) {
-        for (var property in obj) {
-          if (obj.hasOwnProperty(property)) {
-            if (obj[property].constructor == Object) {
-              xmlString += '<' + property + '>';
-              getProps(obj[property]);
-              xmlString += '</' + property + '>';
+          for (var _i = 0; _i < childNodes.length; _i++) {
+            var entryNode = childNodes[_i];
+            var entryObject = {};
+
+            // Skip empty text nodes.
+            if (entryNode.nodeName === '#text') continue;
+
+            var items = entryNode.childNodes;
+
+            for (var _i2 = 0; _i2 < items.length; _i2++) {
+              var item = items[_i2];
+
+              if (item.nodeName === '#text') continue;
+
+              var value = item.innerHTML;
+
+              if (item.nodeName === 'id' || item.nodeName === 'episodes') {
+                value = parseInt(value, 10);
+              }
+
+              entryObject[item.nodeName] = value;
+            }
+
+            if (node.nodeName === 'myanimelist') {
+              if (entryNode.nodeName === 'anime' || entryNode.nodeName === 'manga') {
+                if (!object[node.nodeName][entryNode.nodeName]) {
+                  object[node.nodeName][entryNode.nodeName] = [];
+                }
+                object[node.nodeName][entryNode.nodeName].push(entryObject);
+              } else {
+                object[node.nodeName][entryNode.nodeName] = entryObject;
+              }
             } else {
-              xmlString += '<' + property + '>' + obj[property] + '</' + property + '>';
+              object[node.nodeName].push(entryObject);
             }
           }
         }
+
+        return object;
       }
+    }, {
+      key: '_toXml',
+      value: function _toXml(object) {
+        var xmlString = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
 
-      getProps(object);
+        function getProps(obj) {
+          for (var property in obj) {
+            if (obj.hasOwnProperty(property)) {
+              if (obj[property].constructor == Object) {
+                xmlString += '<' + property + '>';
+                getProps(obj[property]);
+                xmlString += '</' + property + '>';
+              } else {
+                xmlString += '<' + property + '>' + obj[property] + '</' + property + '>';
+              }
+            }
+          }
+        }
 
-      return xmlString;
-    }
-  }, {
-    key: '_get',
-    value: function _get(url) {
-      var _this2 = this;
+        getProps(object);
 
-      return new Promise(function (resolve, reject) {
+        return xmlString;
+      }
+    }, {
+      key: '_get',
+      value: function _get(url) {
+        var _this3 = this;
+
+        return new Promise(function (resolve, reject) {
+          if (isNode) {
+            _this3._getForNode(url, resolve, reject);
+          } else {
+            _this3._getForBrowser(url, resolve, reject);
+          }
+        });
+      }
+    }, {
+      key: '_getForNode',
+      value: function _getForNode(url, resolve, reject) {
+        var _this4 = this;
+
+        request.get(url, function (error, response, data) {
+          if (!error && response.statusCode == 200) {
+            _this4._xmlToJson(data).then(resolve).catch(reject);
+          } else {
+            reject('request failed');
+          }
+        }).auth(this._user, this._password);
+      }
+    }, {
+      key: '_getForBrowser',
+      value: function _getForBrowser(url, resolve, reject) {
+        var _this5 = this;
+
         var req = new XMLHttpRequest();
-
-        req.open('GET', url, true, _this2._user, _this2._password);
-
+        req.open('GET', url, true, this._user, this._password);
         req.onload = function () {
           if (req.status === 200) {
             var data = req.response;
-            var xml = _this2._parseXml(data);
 
-            if (xml) {
-              resolve(_this2._toJson(xml));
-            } else {
-              reject('Failed to parse xml');
-            }
+            // Covert the xml string to json
+            _this5._xmlToJson(data).then(resolve).catch(reject);
           } else {
             reject('request failed');
           }
@@ -230,18 +297,43 @@ var MALjs = function () {
         };
 
         req.send();
-      });
-    }
-  }, {
-    key: '_post',
-    value: function _post(url) {
-      var _this3 = this;
+      }
+    }, {
+      key: '_post',
+      value: function _post(url) {
+        var _this6 = this;
 
-      var data = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+        var data = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
 
-      return new Promise(function (resolve, reject) {
+        return new Promise(function (resolve, reject) {
+          if (isNode) {
+            _this6._postForNode(url, data, resolve, reject);
+          } else {
+            _this6._postForBrowser(url, data, resolve, reject);
+          }
+        });
+      }
+    }, {
+      key: '_postForNode',
+      value: function _postForNode(url, data, resolve, reject) {
+        if (data) {
+          data = this._toXml(data);
+          data = { form: { data: data } };
+        }
+
+        request.post(url, data, function (error, response, data) {
+          if (!error && (response.statusCode == 200 || response.statusCode === 201)) {
+            resolve(data);
+          } else {
+            reject('request failed');
+          }
+        }).auth(this._user, this._password);
+      }
+    }, {
+      key: '_postForBrowser',
+      value: function _postForBrowser(url, data, resolve, reject) {
         var req = new XMLHttpRequest();
-        req.open('POST', url, true, _this3._user, _this3._password);
+        req.open('POST', url, true, this._user, this._password);
         req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
         req.onload = function () {
@@ -257,16 +349,23 @@ var MALjs = function () {
         };
 
         if (data) {
-          var xml = _this3._toXml(data);
+          var xml = this._toXml(data);
           req.send('data=' + xml);
         } else {
           req.send();
         }
-      });
+      }
+    }]);
+
+    return MALjs;
+  }();
+
+  if (isNode) {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = MALjs;
     }
-  }]);
-
-  return MALjs;
-}();
-
-window.MALjs = MALjs;
+    exports.MALjs = MALjs;
+  } else {
+    root.MALjs = MALjs;
+  }
+}).call(undefined);
