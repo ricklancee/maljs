5496e16152dcd578db56b0f20c138ecfa146f42f
Rick Lancee <rickondraw@gmail.com>
2016-12-17 17:04:12 +0100

    add test placeholders

---
+++ parser.test.js
@@ -0,0 +1,5 @@
+import test from 'ava';
+import sinon from 'sinon';
+
+test.todo('test toJson method');
+test.todo('test toXml method');
---
+++ request.test.js
@@ -0,0 +1,7 @@
+import test from 'ava';
+import sinon from 'sinon';
+
+// Use sinon fake server: https://rjzaworski.com/2015/05/testing-api-requests-with-xhr-and-sinon-js
+
+test.todo('test get method');
+test.todo('test post method');
