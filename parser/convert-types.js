2bfaaca1617d07a07dc3daa74f67b2243b88d427
Rick Lancee <rickondraw@gmail.com>
2016-12-18 15:04:42 +0100

    add aditional types and default to string

--- parser/convert-types.js
+++ parser/convert-types.js
@@ -12,7 +12,34 @@ const types = {
   start_date: 'string',
   end_date: 'string',
   synopsis: 'string',
-  image: 'string'
+  image: 'string',
+  user_id: 'int',
+  user_name: 'string',
+  user_watching: 'int',
+  user_completed: 'int',
+  user_onhold: 'int',
+  user_dropped: 'int',
+  user_plantowatch: 'int',
+  user_days_spent_watching: 'float',
+  series_animedb_id: 'int',
+  series_title: 'string',
+  series_synonyms: 'string',
+  series_type: 'int',
+  series_episodes: 'int',
+  series_status: 'int',
+  series_start: 'string',
+  series_end: 'string',
+  series_image: 'string',
+  my_id: 'int',
+  my_watched_episodes: 'int',
+  my_start_date: 'string',
+  my_finish_date: 'string',
+  my_score: 'int',
+  my_status: 'int',
+  my_rewatching: 'int',
+  my_rewatching_ep: 'int',
+  my_last_updated: 'int',
+  my_tags: 'string'
 };
 
 const convertTypes = function(objects) {
@@ -22,22 +49,20 @@ const convertTypes = function(objects) {
     const convertedObject = {};
 
     for(let prop in object) {
-      if (types.hasOwnProperty(prop)) {
-        switch(types[prop]) {
-          case 'int':
-            convertedObject[prop] = parseInt(object[prop]);
-            break;
-          case 'float':
-            convertedObject[prop] = parseFloat(object[prop]);
-            break;
-          case 'date':
-            convertedObject[prop] = new Date(object[prop]);
-            break;
-          case 'string':
-          default:
-            convertedObject[prop] = object[prop];
-            break;
-        }
+      switch(types[prop]) {
+        case 'int':
+          convertedObject[prop] = parseInt(object[prop]);
+          break;
+        case 'float':
+          convertedObject[prop] = parseFloat(object[prop]);
+          break;
+        case 'date':
+          convertedObject[prop] = new Date(object[prop]);
+          break;
+        case 'string':
+        default:
+          convertedObject[prop] = object[prop];
+          break;
       }
     }
 
--- parser/convert-types.test.js
+++ parser/convert-types.test.js
@@ -15,7 +15,34 @@ test('it is should be able to convert stings to the correct types', async t  => 
     start_date: 'string',
     end_date: 'string',
     synopsis: 'string',
-    image: 'string'
+    image: 'string',
+    user_id: '1',
+    user_name: 'string',
+    user_watching: '1',
+    user_completed: '1',
+    user_onhold: '1',
+    user_dropped: '1',
+    user_plantowatch: '1',
+    user_days_spent_watching: '1.1',
+    series_animedb_id: '1',
+    series_title: 'string',
+    series_synonyms: 'string',
+    series_type: '1',
+    series_episodes: '1',
+    series_status: '1',
+    series_start: 'string',
+    series_end: 'string',
+    series_image: 'string',
+    my_id: '1',
+    my_watched_episodes: '1',
+    my_start_date: 'string',
+    my_finish_date: 'string',
+    my_score: '1',
+    my_status: '1',
+    my_rewatching: '1',
+    my_rewatching_ep: '1',
+    my_last_updated: '1',
+    my_tags: 'string'
   }]);
 
   t.deepEqual(converted, [{
@@ -30,6 +57,45 @@ test('it is should be able to convert stings to the correct types', async t  => 
     start_date: 'string',
     end_date: 'string',
     synopsis: 'string',
-    image: 'string'
+    image: 'string',
+    user_id: 1,
+    user_name: 'string',
+    user_watching: 1,
+    user_completed: 1,
+    user_onhold: 1,
+    user_dropped: 1,
+    user_plantowatch: 1,
+    user_days_spent_watching: 1.1,
+    series_animedb_id: 1,
+    series_title: 'string',
+    series_synonyms: 'string',
+    series_type: 1,
+    series_episodes: 1,
+    series_status: 1,
+    series_start: 'string',
+    series_end: 'string',
+    series_image: 'string',
+    my_id: 1,
+    my_watched_episodes: 1,
+    my_start_date: 'string',
+    my_finish_date: 'string',
+    my_score: 1,
+    my_status: 1,
+    my_rewatching: 1,
+    my_rewatching_ep: 1,
+    my_last_updated: 1,
+    my_tags: 'string'
+  }]);
+});
+
+test('when convert an unkown property, default to a string', async t  => {
+  const converted = convertTypes([{
+    id: '1',
+    notknowproperty: '1'
+  }]);
+
+  t.deepEqual(converted, [{
+    id: 1,
+    notknowproperty: '1'
   }]);
 });
